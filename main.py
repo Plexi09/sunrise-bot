5508df9a597e8478bb452ed8d4a3a0af8734db22
Plexi09 <pierre.emmanuel.gobillard@gmail.com>
2024-06-04 23:15:52 +0200

    The bot now pings the user that runs the command

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import time
 import logging
 import logging.handlers
 
@@ -12,7 +14,7 @@ logger.setLevel(logging.DEBUG)
 logging.getLogger('discord.http').setLevel(logging.INFO)
 
 handler = logging.handlers.RotatingFileHandler(
-    filename='discord.log',
+    filename='logs/discord.log',
     encoding='utf-8',
     maxBytes=32 * 1024 * 1024,  # 32 MiB
     backupCount=5,  # Rotate through 5 files
@@ -45,19 +47,24 @@ async def on_message(message):
         return
     
     if message.content.startswith('!ping'):
-        await message.channel.send('Pong!')
+        start_time = time.perf_counter()
+        message_ping = await message.channel.send('Pong!')
+        end_time = time.perf_counter()
+        latency = (end_time - start_time) * 1000  # Convert to milliseconds
+        await message_ping.edit(content=f'Pong! Latency: `{latency:.2f}ms`')
     
     if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+        await message.channel.send(f'Bonjour {message.author.mention} !')
 
     if message.content.startswith('!help'):
-        await message.channel.send('Commandes :`!ping`, `!hello`, `!ip`, `!web`')
+        await message.channel.send(f'Commandes :`!ping`, `!hello`, `!ip`, `!web` \nDemandé par {message.author.mention}')
 
     if message.content.startswith('!ip'):
-        await message.channel.send('IP du serveur: play.sunrisenetwork.eu')
+        await message.channel.send(f'IP du serveur: `play.sunrisenetwork.eu` \nDemandé par {message.author.mention}')
+        await message.delete()
 
     if message.content.startswith('!web'):
-        await message.channel.send('Site web du serveur: https://sunrisenetwork.eu')
+        await message.channel.send(f'Site web du serveur: https://sunrisenetwork.eu \nDemandé par {message.author.mention}')
 
 
 intents = discord.Intents.default()
