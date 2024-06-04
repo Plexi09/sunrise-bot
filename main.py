62e6968a85c1b2502b0b117f8032d6f68234b54d
Plexi09 <pierre.emmanuel.gobillard@gmail.com>
2024-06-04 22:48:46 +0200

    Updated the logging handler

--- main.py
+++ main.py
@@ -2,8 +2,29 @@ import discord
 from dotenv import load_dotenv
 import os
 import logging
+import logging.handlers
 
-handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode="w")
+################################
+############LOGGING#############
+################################
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+################################
+############LOGGING#############
+################################
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
