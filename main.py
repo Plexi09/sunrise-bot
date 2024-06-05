93de489a4bd8227381ff77833048f983ecd7617a
Plexi09 <pierre.emmanuel.gobillard@gmail.com>
2024-06-05 16:57:51 +0200

    Implémentation des commandes slash

--- main.py
+++ main.py
@@ -1,199 +1,133 @@
-import discord
-from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import interactions
 import time
-import logging
-import logging.handlers
-import statistics
 import asyncio
-import random
-import requests
-
-# Configuration des logs
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-logging.getLogger('discord.http').setLevel(logging.INFO)
-
-handler = logging.handlers.RotatingFileHandler(
-    filename='logs/discord.log',
-    encoding='utf-8',
-    maxBytes=32 * 1024 * 1024,  # 32 MiB
-    backupCount=5  # Rotation de 5 fichiers
-)
-
-dt_fmt = '%Y-%m-%d %H:%M:%S'
-formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 # Chargement des variables d'environnement
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+GUILD_ID = int(os.getenv('GUILD_ID'))
 
-# Configuration des intents et du bot
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='!', intents=intents)
+# Configuration du bot et des intents
+bot = interactions.Client(token=BOT_TOKEN)
 
 @bot.event
 async def on_ready():
-    print(f'Connecté en tant que {bot.user}!')
+    print(f'Connecté en tant que ')
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send(f'Commande inconnue. Voici la liste des commandes disponibles:\n`{", ".join([command.name for command in bot.commands])}`')
-    else:
-        await ctx.send(f"Une erreur s'est produite lors de l'exécution de la commande. Veuillez vérifier la console pour plus de détails. Erreur: {error}")
-        raise error
 
-@bot.command(name='ping')
-async def ping(ctx):
-    """: Mesure la latence du bot"""
+@bot.command(
+    name="ip",
+    description="Affiche l'IP du serveur",
+    scope=GUILD_ID,
+)
+async def ip(ctx: interactions.CommandContext):
+    await ctx.send('IP du serveur: `play.sunrisenetwork.eu`')
+    print("Commande exécutée: ping")
+
+
+@bot.command(
+    name="web",
+    description="Affiche l'URL du site web",
+    scope=GUILD_ID,
+)
+async def ip(ctx: interactions.CommandContext):
+    await ctx.send('Site web du serveur: https://sunrisenetwork.eu')
+    print("Commande exécutée: web")
+
+
+@bot.command(
+    name="echo",
+    description="Répète le message de l'utilisateur",
+    scope=GUILD_ID,
+    options = [
+        interactions.Option(
+            name="text",
+            description="Texte a répéter",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ],
+)
+async def echo(ctx: interactions.CommandContext, text: str):
+    await ctx.send(f"{text}")
+
+@bot.command(
+        name="ping",
+        description="Pong",
+        scope=GUILD_ID
+)
+async def ping(ctx: interactions.CommandContext):
     start_time = time.perf_counter()
     message = await ctx.send('Pong!')
     end_time = time.perf_counter()
     latency = (end_time - start_time) * 1000  # Convertir en millisecondes
-    await message.edit(content=f'Pong! Latence: `{latency:.2f}ms`\nDemandé par {ctx.author.mention}')
-    await ctx.message.delete()
+    await message.edit(content=f'Pong! Latence: `{latency:.2f}ms`')
 
-@bot.command(name='hello')
-async def hello(ctx):
-    """: Bonjours !"""
-    await ctx.send(f'Bonjour {ctx.author.mention}!')
 
-@bot.command(name='ip')
-async def ip(ctx):
-    """: Affiche l'IP du serveur"""
-    await ctx.send(f'IP du serveur: `play.sunrisenetwork.eu`\nDemandé par {ctx.author.mention}')
-    await ctx.message.delete()
+@bot.command(
+    name="benchmark",
+    description="Mesure de la latence du bot",
+    scope=GUILD_ID,
+    options=[
+        interactions.Option(
+            name="duration",
+            description="Durée du benchmark en secondes",
+            type=interactions.OptionType.INTEGER,
+            required=True,
+        ),
+    ],
+)
 
-@bot.command(name='web')
-async def web(ctx):
-    """: Affiche l'URL du site web"""
-    await ctx.send(f'Site web du serveur: https://sunrisenetwork.eu\nDemandé par {ctx.author.mention}')
-    await ctx.message.delete()
-
-@bot.command(name='echo')
-async def echo(ctx, *, message: str):
-    """: Répète le message de l'utilisateur"""
-    await ctx.send(message)
-    await ctx.message.delete()
-
-@bot.command(name='random')
-async def random(ctx, min: int = None, max: int = None):
-    """: Génère un nombre aléatoire"""
-    if min is None or max is None:
-        await ctx.send('Usage: `!random <min> <max>`')
-        return
-    
-    await ctx.send("Nombre aléatoire: " + str(random.randint(min, max)))
-    await ctx.message.delete()
-
-@bot.command(name='benchmark')
-async def benchmark(ctx, duration: int = None):
-    """: Mesure de la latence du bot sur une durée donnée (en secondes)."""
-    if duration is None:
-        await ctx.send('Usage: `!benchmark <durée en secondes>`')
-        return
-
+async def benchmark(ctx: interactions.CommandContext, duration: int):
+    print(f"Démarrage du benchmark par {ctx.author.name}")
     latencies = []
 
     start_time = time.time()
     while time.time() - start_time < duration:
         ping_start = time.perf_counter()
-        message = await ctx.send('Benchmarking...')
+        await ctx.send("Benchmarking...")
+        await ctx.message.delete()
         ping_end = time.perf_counter()
-        latency = (ping_end - ping_start) * 1000  # Convertir en millisecondes
+        latency = (ping_end - ping_start) * 1000
         latencies.append(latency)
-        await message.delete()
-        await asyncio.sleep(1)  # Attendre une seconde avant le prochain ping
+        await asyncio.sleep(1)
 
     if latencies:
         min_latency = min(latencies)
         max_latency = max(latencies)
-        avg_latency = statistics.mean(latencies)
-        await ctx.send(f'Résultats du benchmark sur {duration} secondes:\n'
-                       f'Latence Min: `{min_latency:.2f}ms`\n'
-                       f'Latence Max: `{max_latency:.2f}ms`\n'
-                       f'Latence Moyenne: `{avg_latency:.2f}ms`\n'
-                       f'Demandé par {ctx.author.mention}')
+        avg_latency = sum(latencies) / len(latencies)
+        await ctx.send(f"Résultats du benchmark sur {duration} secondes:\n"
+                       f"Latence Min: `{min_latency:.2f}ms`\n"
+                       f"Latence Max: `{max_latency:.2f}ms`\n"
+                       f"Latence Moyenne: `{avg_latency:.2f}ms`")
     else:
-        await ctx.send('Aucune donnée de latence collectée. Veuillez réessayer.')
+        await ctx.send("Aucune donnée de latence collectée.")
 
-def search_google(query):
-    google_api_key = os.getenv('GOOGLE_API_KEY')
-    search_engine_id = '20f02a5ebf8234b5f'
-    url = f'https://www.googleapis.com/customsearch/v1?key={google_api_key}&cx={search_engine_id}&q={query}'
-    response = requests.get(url)
-    search_results = response.json()
-    return search_results
-
-# Commande pour effectuer une recherche sur Google
-@bot.command(name='google')
-async def google(ctx, *, query: str):
-    """: Effectuer une recherche sur Google"""
-    try:
-        search_results = search_google(query)
-        if 'items' in search_results:
-            for result in search_results['items'][:5]:
-                await ctx.send(f"{result['title']}: {result['link']}")
-        else:
-            await ctx.send('Aucun résultat trouvé.')
-    except Exception as e:
-        await ctx.send(f"Une erreur s'est produite lors de la recherche: {e}")
-
-def search_wiki(query):
-    google_api_key = os.getenv('GOOGLE_API_KEY')
-    search_engine_id = '567f0ad4fa4ff419d'
-    url = f'https://www.googleapis.com/customsearch/v1?key={google_api_key}&cx={search_engine_id}&q={query}'
-    response = requests.get(url)
-    search_results = response.json()
-    return search_results
-
-# Commande pour effectuer une recherche sur le wiki
-@bot.command(name='wiki')
-async def google(ctx, *, query: str):
-    """: Effectuer une recherche sur le Wiki"""
-    try:
-        search_results = search_wiki(query)
-        if 'items' in search_results:
-            for result in search_results['items'][:1]:
-                await ctx.send(f"{result['title']}: {result['link']}")
-        else:
-            await ctx.send('Aucun résultat trouvé.')
-    except Exception as e:
-        await ctx.send(f"Une erreur s'est produite lors de la recherche: {e}")
-
-def search_wikipedia(query):
-    url = f'https://fr.wikipedia.org/w/api.php'
-    params = {
-        'action': 'query',
-        'format': 'json',
-        'list': 'search',
-        'srsearch': query,
-        'srlimit': 1,
-    }
-    response = requests.get(url, params=params)
-    search_results = response.json()
-    return search_results
-
-# Commande pour effectuer une recherche sur Wikipédia
-@bot.command(name='ckoi')
-async def wikipedia_search(ctx, *, query: str):
-    """: Effectuer une recherche sur Wikipedia"""
-    try:
-        search_results = search_wikipedia(query)
-        if 'query' in search_results and 'search' in search_results['query'] and search_results['query']['search']:
-            article_title = search_results['query']['search'][0]['title']
-            article_url = f'https://fr.wikipedia.org/wiki/{article_title.replace(" ", "_")}'
-            await ctx.send(f'Voici un lien vers l\'article Wikipedia correspondant: {article_url}')
-        else:
-            await ctx.send('Aucun résultat trouvé sur Wikipédia.')
-    except Exception as e:
-        await ctx.send(f"Une erreur s'est produite lors de la recherche: {e}")
+@bot.command(
+    name="random",
+    description="Génère un nombre aléatoire",
+    scope=GUILD_ID,
+    options=[
+        interactions.Option(
+            name="min",
+            description="Nombre minimum",
+            type=interactions.OptionType.INTEGER,
+            required=True,
+        ),
+            interactions.Option(
+                name="max",
+                description="Nombre maximum",
+                type=interactions.OptionType.INTEGER,
+                required=True,
+        )
+    ]
+)
+async def random(ctx: interactions.CommandContext, duration: int):
+    min = ctx.options[0].value
+    max = ctx.options[1].value
+    await ctx.send(f"Nombre aléatoire: {random.randint(min, max)}")
 
 # Démarrage du bot
-bot.run(BOT_TOKEN)
+bot.start()
