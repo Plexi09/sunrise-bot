bb4685bdadb0800ffe018c78849c817a2385a93e
Plexi09 <pierre.emmanuel.gobillard@gmail.com>
2024-06-06 18:30:51 +0200

    Migration vers l'API v5

--- main.py
+++ main.py
@@ -1,95 +1,79 @@
+import logging
 from dotenv import load_dotenv
 import os
+from interactions import slash_command, SlashContext, OptionType, slash_option, check, is_owner
 import interactions
 import time
 import asyncio
 import random
-import requests
-import sys
+import aiohttp
 
-# Définir l'encodage de sortie sur UTF-8
-sys.stdout.reconfigure(encoding='utf-8')
+# Configurer le logger
+logging.basicConfig(level=logging.INFO)  # Niveau de base pour les messages INFO
+logger = logging.getLogger(__name__) 
 
 # Chargement des variables d'environnement
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 GUILD_ID = int(os.getenv('GUILD_ID'))
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
 # Configuration du bot et des intents
 bot = interactions.Client(token=BOT_TOKEN)
 
+# Définir l'encodage de sortie sur UTF-8
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
+# Événement déclenché lorsque le bot est prêt
 @bot.event
 async def on_ready():
-    print(f'Connecté en tant que {bot.me.name}')
+    logger.info(f'Connecté en tant que {bot.me.name}')
 
-
-@bot.command(
-    name="ip",
-    description="Affiche l'IP du serveur",
-    scope=GUILD_ID,
-)
-async def ip(ctx: interactions.CommandContext):
+# Commande pour afficher l'IP du serveur
+@slash_command(name="ip", description="Affiche l'IP du serveur.")
+async def ip_function(ctx: SlashContext):
     await ctx.send('IP du serveur: `play.sunrisenetwork.eu`')
-    print(f"Commande exécutée par {ctx.author.name}: ip.")
+    logger.info(f"Commande exécutée par {ctx.author.name}: ip.")
 
-
-@bot.command(
-    name="web",
-    description="Affiche l'URL du site web",
-    scope=GUILD_ID,
-)
-async def web(ctx: interactions.CommandContext):
+# Commande pour afficher l'URL du site web
+@slash_command(name="web",description="Affiche l'URL du site web")
+async def web_function(ctx: SlashContext):
     await ctx.send('Site web du serveur: https://sunrisenetwork.eu')
-    print(f"Commande exécutée par {ctx.author.name}: web.")
+    logger.info(f"Commande exécutée par {ctx.author.name}: web.")
 
-
-@bot.command(
-    name="echo",
-    description="Répète le message de l'utilisateur",
-    scope=GUILD_ID,
-    options=[
-        interactions.Option(
+# Commande pour répéter le message de l'utilisateur
+@slash_command(name="echo",description="Répète le message de l'utilisateur")
+@slash_option(
             name="text",
             description="Texte à répéter",
-            type=interactions.OptionType.STRING,
+            opt_type=OptionType.STRING,
             required=True,
-        ),
-    ],
-)
-async def echo(ctx: interactions.CommandContext, text: str):
+        )
+async def echo_function(ctx: SlashContext, text: str):
     await ctx.send(f"{text}")
-    print(f"Commande exécutée par {ctx.author.name}: echo.")
+    logger.info(f"Commande exécutée par {ctx.author}: echo. Message: {text}")
 
-
-@bot.command(
-    name="ping",
-    description="Pong",
-    scope=GUILD_ID
-)
-async def ping(ctx: interactions.CommandContext):
+# Commande pour afficher "Pong!" avec la latence
+@slash_command(name="ping",description="Pong")
+async def ping_function(ctx: SlashContext):
     start_time = time.perf_counter()
     message = await ctx.send('Pong!')
     end_time = time.perf_counter()
     latency = (end_time - start_time) * 1000  # Convertir en millisecondes
     await message.edit(content=f'Pong! Latence: `{latency:.2f}ms`')
-    print(f"Commande exécutée par {ctx.author.name}: ping. Latence: `{latency:.2f}ms`")
+    logger.info(f"Commande exécutée par {ctx.author}: ping. Latence: `{latency:.2f}ms`")
 
-
-@bot.command(
-    name="benchmark",
-    description="Mesure de la latence du bot",
-    scope=GUILD_ID,
-    options=[
-        interactions.Option(
-            name="duration",
-            description="Durée du benchmark en secondes",
-            type=interactions.OptionType.INTEGER,
-            required=True,
-        ),
-    ],
-)
-async def benchmark(ctx: interactions.CommandContext, duration: int):
-    print(f"Démarrage du benchmark par {ctx.author.name}")
+# Commande pour effectuer un benchmark de latence
+@slash_command(name="benchmark",description="Mesure de la latence du bot")
+@slash_option(
+    name="duration",
+    description="Durée du benchmark en secondes",
+    opt_type=OptionType.INTEGER,
+    required=True,
+    )
+async def benchmark_function(ctx: SlashContext, duration: int):
+    logger.info(f"Démarrage du benchmark par {ctx.author}")
     latencies = []
 
     start_time = time.time()
@@ -110,81 +94,67 @@ async def benchmark(ctx: interactions.CommandContext, duration: int):
                        f"Latence Min: `{min_latency:.2f}ms`\n"
                        f"Latence Max: `{max_latency:.2f}ms`\n"
                        f"Latence Moyenne: `{avg_latency:.2f}ms`")
-        print(f"Fin du benchmark sur {duration}. Latency min: `{min_latency:.2f}ms. Latency max: `{max_latency:.2f}ms. Latency avg: `{avg_latency:.2f}ms")
+        logger.info(f"Fin du benchmark sur {duration} secondes. Latency min: `{min_latency:.2f}ms. Latency max: `{max_latency:.2f}ms. Latency avg: `{avg_latency:.2f}ms")
     else:
         await ctx.send("Aucune donnée de latence collectée.")
+        logger.warning(f"Aucune donnée de latence collectée lors du benchmark par {ctx.author}")
 
-
-@bot.command(
-    name="random",
-    description="Génère un nombre aléatoire",
-    scope=GUILD_ID,
-    options=[
-        interactions.Option(
-            name="min",
-            description="Nombre minimum",
-            type=interactions.OptionType.INTEGER,
-            required=True,
-        ),
-        interactions.Option(
-            name="max",
-            description="Nombre maximum",
-            type=interactions.OptionType.INTEGER,
-            required=True,
-        )
-    ]
+# Commande pour générer un nombre aléatoire entre min et max
+@slash_command(name="random",description="Génère un nombre aléatoire")
+@slash_option(
+    name="min",
+    description="Nombre minimum",
+    opt_type=OptionType.INTEGER,
+    required=True,
 )
-async def random_command(ctx: interactions.CommandContext, min: int, max: int):
+@slash_option(
+    name="max",
+    description="Nombre maximum",
+    opt_type=OptionType.INTEGER,
+    required=True,
+)
+async def random_function(ctx: SlashContext, min: int, max: int):
     randomnumber = random.randint(min, max)
     await ctx.send(f"Nombre aléatoire: {randomnumber}")
-    print(f"Commande exécutée par {ctx.author.name}: random. Nombre aléatoire: {randomnumber}")
+    logger.info(f"Commande exécutée par {ctx.author}: random. Nombre aléatoire: {randomnumber}")
 
-
-@bot.command(
-    name="google",
-    description="Recherche sur Google",
-    scope=GUILD_ID,
-    options=[
-        interactions.Option(
-            name="recherche",
-            description="Recherche à effectuer",
-            type=interactions.OptionType.STRING,
-            required=True
-        )
-    ]
+# Commande pour rechercher sur Google
+@slash_command(name="google",description="Recherche sur Google")
+@slash_option(
+    name="recherche",
+    description="Recherche à effectuer",
+    opt_type=OptionType.STRING,
+    required=True
 )
-async def google(ctx: interactions.CommandContext, recherche: str):
-    google_api_key = os.getenv('GOOGLE_API_KEY')
+async def google_function(ctx: SlashContext, recherche: str):
     search_engine_id = '20f02a5ebf8234b5f'
-    url = f'https://www.googleapis.com/customsearch/v1?key={google_api_key}&cx={search_engine_id}&q={recherche}'
-    response = requests.get(url)
-    search_results = response.json()
+    url = f'https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={search_engine_id}&q={recherche}'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            search_results = await response.json()
+
     try:
         if 'items' in search_results:
             for result in search_results['items'][:1]:
                 await ctx.send(f"{result['title']}: {result['link']}")
-                print(f"Commande exécutée par {ctx.author.name}: Google. Recherche: {recherche}")
+                logger.info(f"Commande exécutée par {ctx.author}: Google. Recherche: {recherche}")
         else:
             await ctx.send('Aucun résultat trouvé.')
+            logger.info(f"Aucun résultat trouvé pour la recherche '{recherche}' par {ctx.author}")
     except Exception as e:
         await ctx.send(f"Une erreur s'est produite lors de la recherche: {e}")
-        print(f"Une erreur s'est produite lors de la recherche: {e}")
+        logger.error(f"Une erreur s'est produite lors de la recherche: {e}")
 
-
-@bot.command(
-    name="wikipedia",
-    description="Recherche sur Wikipedia",
-    scope=GUILD_ID,
-    options=[
-        interactions.Option(
-            name="recherche",
-            description="Recherche à effectuer",
-            type=interactions.OptionType.STRING,
-            required=True
-        )
-    ]
+# Commande pour rechercher sur Wikipedia
+@slash_command(name="wikipedia",description="Recherche sur Wikipedia")
+@slash_option(
+    name="recherche",
+    description="Recherche à effectuer",
+    opt_type=OptionType.STRING,
+    required=True
 )
-async def wikipedia(ctx: interactions.CommandContext, recherche: str):
+async def wikipedia_function(ctx: SlashContext, recherche: str):
     url = f'https://fr.wikipedia.org/w/api.php'
     params = {
         'action': 'query',
@@ -193,52 +163,51 @@ async def wikipedia(ctx: interactions.CommandContext, recherche: str):
         'srsearch': recherche,
         'srlimit': 1,
     }
-    response = requests.get(url, params=params)
-    search_results = response.json()
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params) as response:
+            search_results = await response.json()
+
     try:
         if 'query' in search_results and 'search' in search_results['query'] and search_results['query']['search']:
             article_title = search_results['query']['search'][0]['title']
             article_url = f'https://fr.wikipedia.org/wiki/{article_title.replace(" ", "_")}'
             await ctx.send(f'Voici un lien vers l\'article Wikipedia correspondant: {article_url}')
-            print(f"Commande exécutée par {ctx.author.name}: Wikipedia. Recherche: {recherche}")
+            logger.info(f"Commande exécutée par {ctx.author}: Wikipedia. Recherche: {recherche}")
         else:
             await ctx.send('Aucun résultat trouvé sur Wikipédia.')
+            logger.info(f"Aucun résultat trouvé sur Wikipédedia pour la recherche '{recherche}' par {ctx.author}")
     except Exception as e:
         await ctx.send(f"Une erreur s'est produite lors de la recherche: {e}")
-        print(f"Une erreur s'est produite lors de la recherche: {e}")
-    
-@bot.command(
-    name="wiki",
-    description="Recherche sur le Wiki",
-    scope=GUILD_ID,
-    options=[
-        interactions.Option(
-            name="recherche",
-            description="Recherche à effectuer",
-            type=interactions.OptionType.STRING,
-            required=True
-        )
-    ]
+        logger.error(f"Une erreur s'est produite lors de la recherche: {e}")
+
+# Commande pour rechercher sur le Wiki
+@slash_command(name="wiki", description="Recherche sur le Wiki")
+@slash_option(
+    name="recherche",
+    description="Recherche à effectuer",
+    opt_type=OptionType.STRING,
+    required=True
 )
-async def wiki(ctx: interactions.CommandContext, recherche: str):
+async def wiki_function(ctx: SlashContext, recherche: str):
+    search_engine_id = '567f0ad4fa4ff419d'
+    url = f'https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={search_engine_id}&q={recherche}'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            search_results = await response.json()
+
     try:
-        search_results = search_google(recherche)
         if 'items' in search_results:
             for result in search_results['items'][:1]:
                 await ctx.send(f"{result['title']}: {result['link']}")
+                logger.info(f"Commande exécutée par {ctx.author}: Google. Recherche: {recherche}")
         else:
             await ctx.send('Aucun résultat trouvé.')
-            print(f"Commande exécutée par {ctx.author.name}: Wiki. Recherche: {recherche}")
+            logger.info(f"Aucun résultat trouvé pour la recherche '{recherche}' par {ctx.author}")
     except Exception as e:
         await ctx.send(f"Une erreur s'est produite lors de la recherche: {e}")
-        print(f"Une erreur s'est produite lors de la recherche: {e}")
+        logger.error(f"Une erreur s'est produite lors de la recherche: {e}")
 
-def search_google(query: str):
-    google_api_key = os.getenv('GOOGLE_API_KEY')
-    search_engine_id = '20f02a5ebf8234b5f'
-    url = f'https://www.googleapis.com/customsearch/v1?key={google_api_key}&cx={search_engine_id}&q={query}'
-    response = requests.get(url)
-    return response.json()
-
-
+# Démarrage du bot
 bot.start()
